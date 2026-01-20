eb9264b6cc1f0df0f1d442e8351cb4015bb529e8
darmohsin63 <168063173+darmohsin63@users.noreply.github.com>
2026-01-20 22:14:43 +0530

    Switch from BrowserRouter to HashRouter
    Replaced BrowserRouter with HashRouter for mobile APK support.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// CHANGED: Imported HashRouter instead of BrowserRouter
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
 import { AuthProvider } from "@/hooks/useAuth";
 import { AnimatedLoader } from "@/components/AnimatedLoader";
@@ -39,7 +40,8 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        {/* CHANGED: Switched to HashRouter for mobile APK support */}
+        <HashRouter>
           <AuthProvider>
             <AdManager />
             <Suspense fallback={<AnimatedLoader />}>
@@ -67,7 +69,7 @@ const App = () => (
               </Routes>
             </Suspense>
           </AuthProvider>
-        </BrowserRouter>
+        </HashRouter>
       </TooltipProvider>
     </ThemeProvider>
   </QueryClientProvider>
